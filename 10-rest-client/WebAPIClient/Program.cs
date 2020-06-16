bd2c1e544e71050a3596e4339028cf06dd99104f
junnac <bigdogstore@gmail.com>
2020-06-16 14:43:12 -0700

    Finish tutorial to fetch and parse json from GitHub API

---
+++ 10-rest-client/WebAPIClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            var repositories = await ProcessRepositories();
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine(repo.LastPush);
+                Console.WriteLine();
+            }
+        }
+
+        private static async Task<List<Repository>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            // Configure to accept GitHub JSON responses
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            // Add a User Agent header to all requests
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            
+            // var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+            // var msg = await stringTask;
+            // Console.Write(msg);
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+            // foreach (var repo in repositories)
+            //     Console.WriteLine(repo.Name);
+
+            return repositories;
+        }
+    }
+}
---
+++ 10-rest-client/WebAPIClient/Repository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace WebAPIClient
+{
+    public class Repository
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public Uri GitHubHomeUrl { get; set; }
+
+        [JsonPropertyName("homepage")]
+        public Uri Homepage { get; set; }
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public DateTime LastPushUtc { get; set; }
+        public DateTime LastPush => LastPushUtc.ToLocalTime();
+    }
+}
